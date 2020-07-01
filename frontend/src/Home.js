be274420ff41c0008885ed8922169d2613c2e5ff
Kesava Karri <kkeshav1998@gmail.com>
2020-07-01 11:39:48 +0530

    updated home.js

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,15 +1,33 @@
-import React from "react";
-import { Paper } from "@material-ui/core";
+// import React from "react";
+// // import { Paper } from "@material-ui/core";
+
+// const Home = (props) => {
+//   return (
+//     <div style={{ overflow: "hidden" }}>
+//       {/* <Paper onClick={() => props.history.push("/dashboard")}>Home</Paper> */}
+//       <video id="bg-video" loop autoPlay>
+//         <source src="./assets/bg-video.mp4" type="video/mp4" />
+//       </video>
+//     </div>
+//   );
+// };
+
+import React from 'react'
+import ReactPlayer from 'react-player'
+import screenfull from 'screenfull'
 
 const Home = (props) => {
-  return (
-    <div style={{ overflow: "hidden" }}>
-      <Paper onClick={() => props.history.push("/dashboard")}>Home</Paper>
-      <video id="bg-video" loop autoPlay>
-        <source src="./assets/bg-video.mp4" type="video/mp4" />
-      </video>
-    </div>
-  );
-};
+    return (
+        <ReactPlayer 
+        // url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
+        // url='https://youtu.be/I-Yd-_XIWJg?start=182&end=267'
+        url='https://youtu.be/ZzlMEKvQwYg'
+        loop = {true}
+        width='100%'
+        height='100%'
+        />
+        // screenfull.request(url)
+    )
 
-export default Home;
+    }
+export default Home
