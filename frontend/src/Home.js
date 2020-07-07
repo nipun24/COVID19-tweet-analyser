89db9e3c926596801724aad13fde7fab02ebfc69
Kesava Karri <kkeshav1998@gmail.com>
2020-07-07 23:29:43 +0530

    given_id_to_home_and_used_grid

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,35 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Home.css'
-import { Typography, makeStyles } from '@material-ui/core';
+import { Typography, makeStyles, Grid } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles({
-  button: {
-    borderRadius: 100,
-    marginTop:320,
-    marginLeft:670,
-  },
-  heading:{
-    color:"black"
-  }
-})
+    heading:{
+      color:"black"
+    }
+  })
+
 export default function Home() {
   const classes = useStyles();
     return(
-        <div className={classes.button}>
-          <Typography 
-            className={classes.heading}
-            variant='h3'
-          >
-            Heading
-          </Typography>
-          <Button 
-            variant="contained"
-            color="primary"
-          >
-            Go to Dashboard
-          </Button>
-          
+        <div id = "gg" >
+            <Grid
+                style={{ height: "90vh" }}
+                container
+                direction="column"
+                justify="center"
+                alignItems="center"
+            >
+                <Typography 
+                    className={classes.heading}
+                    variant='h3'
+                >
+                    Heading
+                </Typography>
+                <Button 
+                    variant="contained"
+                    color="primary"
+                >
+                    Go to Dashboard
+                </Button>
+          </Grid>
         </div>
     )
 }
