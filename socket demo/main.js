064262ce3fc52c1dc52e53dac4f6659fb9d20650
Nipun Haldar <nipunhaldar24@gmail.com>
2020-07-08 17:45:47 +0530

    Removed Stringify

--- socket demo/main.js
+++ socket demo/main.js
@@ -1,37 +1,39 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = app.listen(3000,()=>{console.log('localhost:3000')});
+const server = app.listen(3000, () => {
+  console.log("localhost:3000");
+});
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-const io = require('socket.io')(server);
-const sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database('./twitter.db');
+const io = require("socket.io")(server);
+const sqlite3 = require("sqlite3").verbose();
+let db = new sqlite3.Database("./twitter.db");
 
 const sendPie = () => {
-    let sql = 'SELECT * FROM sentiment';
-    var obj = [{id:'positive',label:'positive',value:0},
-              {id:'negative',label:'negative',value:0},
-              {id:'neutral',label:'neutral',value:0}]
-    db.all(sql, [], (err, rows) => {
-        if (err) {
-          throw err;
-        }
-        rows.forEach((row) => {
-          var sentiment = row.sentiment;
-          if (sentiment>=0.2){
-            obj[0].value++;
-          }
-          else if (sentiment <= -0.2){
-            obj[1].value++;
-          }
-          else {
-            obj[2].value++;
-          }
-        });
-        io.emit('pie',JSON.stringify(obj));
-        // console.log(obj);
-      });
-}
+  let sql = "SELECT * FROM sentiment";
+  var obj = [
+    { id: "positive", label: "positive", value: 0 },
+    { id: "negative", label: "negative", value: 0 },
+    { id: "neutral", label: "neutral", value: 0 },
+  ];
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    rows.forEach((row) => {
+      var sentiment = row.sentiment;
+      if (sentiment >= 0.2) {
+        obj[0].value++;
+      } else if (sentiment <= -0.2) {
+        obj[1].value++;
+      } else {
+        obj[2].value++;
+      }
+    });
+    io.emit("pie", obj);
+    // console.log(obj);
+  });
+};
 
-io.on('connection',() => sendPie())
+io.on("connection", () => sendPie());
