950dd79a6988dcf03c34344c00aae7735fd7be81
Nipun Haldar <nipunhaldar24@gmail.com>
2020-07-08 12:23:57 +0530

    Removed errors from merge

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,40 +1,37 @@
-import React, { Component } from 'react';
-import './Home.css'
-import { Typography, makeStyles, Grid } from '@material-ui/core';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import "./Home.css";
+import { Typography, makeStyles, Grid, Button } from "@material-ui/core";
 
 const useStyles = makeStyles({
-    heading:{
-      color:"black"
-    }
-  })
+  heading: {
+    color: "black",
+  },
+});
 
-export default function Home() {
+const Home = (props) => {
   const classes = useStyles();
-    return(
-        <div id = "background" >
-            <Grid
-                style={{ height: "90vh" }}
-                container
-                direction="column"
-                justify="center"
-                alignItems="center"
-            >
-                <Typography 
-                    className={classes.heading}
-                    variant='h3'
-                >
-                    Heading
-                </Typography>
-                <Button 
-                    variant="contained"
-                    color="primary"
-                >
-                    Go to Dashboard
-                </Button>
-          </Grid>
-        </div>
-    )
-}
+  return (
+    <div id="background">
+      <Grid
+        style={{ height: "90vh" }}
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+      >
+        <Typography className={classes.heading} variant="h3">
+          Heading
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => props.history.push("/dashboard")}
+        >
+          Go to Dashboard
+        </Button>
+      </Grid>
+    </div>
+  );
+};
 
 export default Home;
